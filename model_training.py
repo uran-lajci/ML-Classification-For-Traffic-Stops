d6143f43a7aacedb8fb5f79e555a76640bdf2e32
elvirmisini <elvirmisini@gmail.com>
2023-03-28 00:07:39 +0200

    Added confusion matrix and classification report

--- model_training.py
+++ model_training.py
@@ -7,6 +7,9 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 df = pd.read_csv("preprocessed_police_project.csv")
 
@@ -36,35 +39,99 @@ else:
 
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
 
-training_model = str(input("""Write LR for Logistic Regression,
-      DTC for Decision Tree Classifier,
-      RFC for Random Forest Classifier,
-      NB for Gaussian Naive Bayes,
-      KNN for KNeighbors Classifier
-      Model: """))
+# training_model = str(input("""Write LR for Logistic Regression,
+#       DTC for Decision Tree Classifier,
+#       RFC for Random Forest Classifier,
+#       NB for Gaussian Naive Bayes,
+#       KNN for KNeighbors Classifier
+#       Model: """))
 
 # Initialize the model
-if training_model == "LR":
-    model = LogisticRegression()
-elif training_model == "DTC":
-    model = DecisionTreeClassifier()
-elif training_model == "RFC":
-    model = RandomForestClassifier()
-elif training_model == "NB":
-    model = GaussianNB()
-elif training_model == "KNN":
-    model = KNeighborsClassifier()
-else:
-    print(f"Error. Wrong input {training_model}")
+# if training_model == "LR":
+#     model = LogisticRegression()
+# elif training_model == "DTC":
+#     model = DecisionTreeClassifier()
+# elif training_model == "RFC":
+#     model = RandomForestClassifier()
+# elif training_model == "NB":
+#     model = GaussianNB()
+# elif training_model == "KNN":
+#     model = KNeighborsClassifier()
+# else:
+#     print(f"Error. Wrong input {training_model}")
 
+
+
+LRmodel = LogisticRegression()
+DTmodel = DecisionTreeClassifier()
+RFmodel = RandomForestClassifier()
+NBmodel = GaussianNB()
+KNNmodel = KNeighborsClassifier()
 # Train the model on the training data
-model.fit(X_train, y_train)
+#model.fit(X_train, y_train)
+LRmodel.fit(X_train, y_train)
+DTmodel.fit(X_train, y_train)
+RFmodel.fit(X_train, y_train)
+NBmodel.fit(X_train, y_train)
+KNNmodel.fit(X_train, y_train)
 
-# Make predictions on the testing data
-y_pred = model.predict(X_test)
+#Make predictions on the testing data
+y_pred = LRmodel.predict(X_test)
+accuracyLR = round(accuracy_score(y_test, y_pred)*100,2)
+print("Logistic Regression : %f" %accuracyLR)
+print(confusion_matrix(y_test, y_pred))
+print('================')
+print(classification_report(y_test, y_pred))
+print('====================================================')
+print('====================================================')
+
+
+y_pred = DTmodel.predict(X_test)
+accuracyDT = round(accuracy_score(y_test, y_pred)*100,2)
+print("Decision Tree : %f" %accuracyDT)
+print(confusion_matrix(y_test, y_pred))
+print('================')
+print(classification_report(y_test, y_pred))
+print('====================================================')
+print('====================================================')
+
+y_pred = RFmodel.predict(X_test)
+accuracyRF = round(accuracy_score(y_test, y_pred)*100,2)
+print("Random Forest : %f" %accuracyRF)
+print(confusion_matrix(y_test, y_pred))
+print('================')
+print(classification_report(y_test, y_pred))
+print('====================================================')
+print('====================================================')
+
+
+y_pred = NBmodel.predict(X_test)
+accuracyNB = round(accuracy_score(y_test, y_pred)*100,2)
+print("Naive Bayes : %f" %accuracyNB)
+print(confusion_matrix(y_test, y_pred))
+print('================')
+print(classification_report(y_test, y_pred))
+print('====================================================')
+print('====================================================')
+
+y_pred = KNNmodel.predict(X_test)
+accuracyKNN = round(accuracy_score(y_test, y_pred)*100,2)
+print("KNN : %f" %accuracyKNN)
+print(confusion_matrix(y_test, y_pred))
+print('================')
+print(classification_report(y_test, y_pred))
+print('====================================================')
+print('====================================================')
+
+# y_pred = model.predict(X_test)
+# accuracy = round(accuracy_score(y_test, y_pred)*100,2)
+# print("KNN : %f" %accuracy)
+# print(confusion_matrix(y_test, y_pred))
+# print('================')
+# print(classification_report(y_test, y_pred))
+# print('====================================================')
+# print('====================================================')
 
 # Evaluate the model's performance
-accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
