5bf7c9fdec5f1823105f4fc83e25966b07c8c26c
elvirmisini <elvirmisini@gmail.com>
2023-04-10 18:13:26 +0200

    Added atribute violation to be predicted, selected features differently

--- model_training.py
+++ model_training.py
@@ -23,7 +23,7 @@ df = pd.read_csv("preprocessed_police_project.csv")
 number_of_rows_to_use_for_test_and_train = int(input(f"Write the number of rows you want to use from {len(df)} that are in the dataset: "))
 df = df[0:number_of_rows_to_use_for_test_and_train]
 
-attribute_to_be_predicted = str(input("Chose wich attribute you want to predict from these attributes: stop_outcome, driver_gender, age_group, is_arrested\nAttribute to be predicted: "))
+attribute_to_be_predicted = str(input("Chose wich attribute you want to predict from these attributes: stop_outcome, driver_gender, age_group, is_arrested or violation\nAttribute to be predicted: "))
 
 if attribute_to_be_predicted == "stop_outcome":
     # Extract the features and target variable
@@ -43,20 +43,29 @@ elif attribute_to_be_predicted == "is_arrested":
     X = pd.get_dummies(X, columns=['driver_gender', 'driver_race', 'violation', 'stop_outcome', 'stop_duration', 'stop_datetime', 'age_group'])
 elif attribute_to_be_predicted == "driver_gender":
     # Extract the features and target variable
-    X = df[[ 'driver_race', 'violation','is_arrested', 'search_conducted',
-                'stop_outcome', 'stop_duration', 'drugs_related_stop', 'stop_datetime', 'age_group']]
+    X = df[['violation','is_arrested', 'search_conducted',
+                 'stop_duration', 'drugs_related_stop',  'age_group']]
     y = df['driver_gender']
 
     # Encode categorical variables as numbers
-    X = pd.get_dummies(X, columns=[ 'driver_race', 'violation','is_arrested', 'stop_outcome', 'stop_duration', 'stop_datetime', 'age_group'])
+    X = pd.get_dummies(X, columns=['violation','is_arrested',  'stop_duration',  'age_group'])
+
 elif attribute_to_be_predicted == "age_group":
     # Extract the features and target variable
     X = df[[ 'driver_gender','driver_race', 'violation','is_arrested', 'search_conducted',
-                'stop_outcome', 'stop_duration', 'drugs_related_stop', 'stop_datetime', ]]
+                'stop_outcome', 'stop_duration', 'drugs_related_stop', ]]
     y = df['age_group']
 
     # Encode categorical variables as numbers
-    X = pd.get_dummies(X, columns=[ 'driver_gender','driver_race', 'violation','is_arrested', 'stop_outcome', 'stop_duration', 'stop_datetime'])
+    X = pd.get_dummies(X, columns=[ 'driver_gender','driver_race', 'violation','is_arrested', 'stop_outcome', 'stop_duration'])
+elif attribute_to_be_predicted == "violation":
+    # Extract the features and target variable
+    X = df[[ 'driver_gender','driver_race', 'search_conducted',
+                'stop_outcome','drugs_related_stop', 'age_group' ]]
+    y = df['violation']
+
+    # Encode categorical variables as numbers
+    X = pd.get_dummies(X, columns=[ 'driver_gender','driver_race', 'stop_outcome','age_group'])
 else:
     print(f"Error. Wrong input {attribute_to_be_predicted}")
 
@@ -103,24 +112,24 @@ print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 
+if(attribute_to_be_predicted=='driver_gender' or attribute_to_be_predicted=='is_arrested'):
 
+    # Convert categorical labels to binary labels
+    le = LabelEncoder()
+    y_test = le.fit_transform(y_test)
 
-# Convert categorical labels to binary labels
-le = LabelEncoder()
-y_test = le.fit_transform(y_test)
+    # Get the predicted probabilities
+    y_score = model.predict_proba(X_test)[:, 1]
 
-# Get the predicted probabilities
-y_score = model.predict_proba(X_test)[:, 1]
+    # Compute the ROC curve and AUC score
+    fpr, tpr, thresholds = roc_curve(y_test, y_score)
+    auc_score = roc_auc_score(y_test, y_score)
 
-# Compute the ROC curve and AUC score
-fpr, tpr, thresholds = roc_curve(y_test, y_score)
-auc_score = roc_auc_score(y_test, y_score)
-
-# Plot the ROC curve
-plt.plot(fpr, tpr, label=f'AUC = {auc_score:.2f}')
-plt.plot([0, 1], [0, 1], linestyle='--', color='r', label='Random')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC Curve')
-plt.legend()
-plt.show()
+    # Plot the ROC curve
+    plt.plot(fpr, tpr, label=f'AUC = {auc_score:.2f}')
+    plt.plot([0, 1], [0, 1], linestyle='--', color='r', label='Random')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend()
+    plt.show()
